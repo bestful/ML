3e70ff4b4c0928e95c44a986df9b3aff7f74d5ea
bestful <kataev.i@gmail.com>
2017-11-18 15:02:16 +0300

    ^_^

--- lib/alpha.r
+++ lib/alpha.r
@@ -28,8 +28,8 @@ mc.stolp(xl, l0, delta1, delta0, f, ot, ...){
   
 }
 
-learn.gamma.gen <- function(f, xl, by, ...){
-  g <- learn.gamma(f, xl, by, ...)
+learn.gamma.gen <- function(f, xl,  ...){
+  g <- learn.gamma(f, xl,  ...)
   gen <- f
   formals(gen) <- formals(f)[-3]
   en <- environment(gen)
